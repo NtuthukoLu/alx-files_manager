44a1be8f5d074bf38c196ad1865dcaebee422b21
Ntuthuko <ntuthukolu@gmail.com>
2024-05-15 23:32:16 +0200

    task3

---
+++ controllers/UsersController.js
@@ -0,0 +1,56 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+const UsersController = {
+  async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      const user = await dbClient.getUserByEmail(email);
+      if (user) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const hashedPwd = sha1(password);
+      const newUser = await dbClient.createUser(email, hashedPwd);
+
+      res.status(201).json({ email: newUser.email, id: newUser._id });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+    return null;
+  },
+
+  async getMe(req, res) {
+    const token = req.header('X-Token');
+    if (!token) {
+      return res.status(400).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(400).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.getUserById(userId);
+
+    if (!user) {
+      return res.status(400).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ email: user.email, id: user._id });
+  },
+};
+
+module.exports = UsersController;
